333cdf04aa78e7c38d992ea4b9ab30c88e8e1606
ContinuumCollapse <interloperprevention@gmail.com>
2025-09-12 13:27:37 -0700

    Cinemachine Camera System

---
+++ Unity-Solo-Project/Assets/Scripts/NextBot.cs
@@ -0,0 +1,17 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject Enemy;
+    public GameObject Player;
+    public float speed;
+
+    void Update() 
+    {
+        Enemy.transform.position = Vector3.MoveTowards(Enemy.transform.position, Player.transform.position, speed);
+    
+    
+    
+    }
+}
+
