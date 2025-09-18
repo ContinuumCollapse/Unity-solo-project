cb39dbad4ad50339e04dfb81cc1cf8c6766397d0
ContinuumCollapse <interloperprevention@gmail.com>
2025-09-18 13:21:20 -0700

    Enemy Death

--- Unity-Solo-Project/Assets/Scripts/EnemyControl.cs
+++ Unity-Solo-Project/Assets/Scripts/EnemyControl.cs
@@ -1,19 +1,51 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 public class EnemyControl : MonoBehaviour
 {
     NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int health = 5;
+    public int maxhealth = 5;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.destination = GameObject.Find("Player").transform.position;
+       
 
     }
-
-    // Update is called once per frame
-    void Update()
+    
+        // Update is called once per frame
+        private void Update()
     {
         agent.destination = GameObject.Find("Player").transform.position;
+        
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "KillZone")
+        {
+            health = 0;
+
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision) //enter is once every collison, stay is constant while collision is true
+    {
+        if (collision.gameObject.tag == "Hazard")
+        {
+            health--;
+        }
+    }
+    private void OnCollisionStay(Collision collision) //enter is once every collison, stay is constant while collision is true
+    {
+        if (collision.gameObject.tag == "Hazard2")
+        {
+            health--;
+        }
     }
 }
